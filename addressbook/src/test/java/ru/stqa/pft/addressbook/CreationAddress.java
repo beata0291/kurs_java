5275f2fedad30088922118cbe30d02213ed7318e
beata0291 <beata.dabrowska91Qgmail.com>
2017-12-16 23:00:22 +0100

    dodanie ApplicationManager

--- addressbook/src/test/java/ru/stqa/pft/addressbook/CreationAddress.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/CreationAddress.java
@@ -1,13 +1,7 @@
 package ru.stqa.pft.addressbook;
 
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
 
 public class CreationAddress extends TestBase{
@@ -24,36 +18,36 @@ public class CreationAddress extends TestBase{
     }
 
     private void returnAllAddress() {
-        wd.findElement(By.linkText("home page")).click();
+        app.wd.findElement(By.linkText("home page")).click();
     }
 
     private void submitAddress() {
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+        app.wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     }
 
     private void fillAddressForm(GroupAdressData groupAdressData) {
-        wd.findElement(By.name("firstname")).sendKeys(groupAdressData.getFirstName());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(groupAdressData.getLastName());
-        wd.findElement(By.name("address")).click();
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(groupAdressData.getAddress());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(groupAdressData.getEmail());
-        wd.findElement(By.name("mobile")).click();
-        wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys(groupAdressData.getPhones());
+        app.wd.findElement(By.name("firstname")).sendKeys(groupAdressData.getFirstName());
+        app.wd.findElement(By.name("lastname")).click();
+        app.wd.findElement(By.name("lastname")).clear();
+        app.wd.findElement(By.name("lastname")).sendKeys(groupAdressData.getLastName());
+        app.wd.findElement(By.name("address")).click();
+        app.wd.findElement(By.name("address")).clear();
+        app.wd.findElement(By.name("address")).sendKeys(groupAdressData.getAddress());
+        app.wd.findElement(By.name("email")).click();
+        app.wd.findElement(By.name("email")).clear();
+        app.wd.findElement(By.name("email")).sendKeys(groupAdressData.getEmail());
+        app.wd.findElement(By.name("mobile")).click();
+        app.wd.findElement(By.name("mobile")).clear();
+        app.wd.findElement(By.name("mobile")).sendKeys(groupAdressData.getPhones());
     }
 
     private void initAddressCreation() {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
+        app.wd.findElement(By.name("firstname")).click();
+        app.wd.findElement(By.name("firstname")).clear();
     }
 
     private void goToNewAddressPage() {
-        wd.findElement(By.linkText("add new")).click();
+        app.wd.findElement(By.linkText("add new")).click();
     }
 
 
--- addressbook/src/test/java/ru/stqa/pft/addressbook/CreationGroupTest.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/CreationGroupTest.java
@@ -1,25 +1,17 @@
 package ru.stqa.pft.addressbook;
 
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
 
 public class CreationGroupTest extends TestBase {
 
     @Test
     public void testCreationGroup() {
 
-        goToGroupPage();
-        initGroupCreation();
-        fillGroupForm(new GroupData("Polska", "małopolska", "Kraków"));
-        submitGroup();
-        returnToAllGroup();
+        app.goToGroupPage();
+        app.initGroupCreation();
+        app.fillGroupForm(new GroupData("Polska", "małopolska", "Kraków"));
+        app.submitGroup();
+        app.returnToAllGroup();
     }
 
 }
