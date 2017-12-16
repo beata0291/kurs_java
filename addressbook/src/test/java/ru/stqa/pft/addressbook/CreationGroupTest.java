3182e3b65211ad50f3f5a51bb71925deda2344c8
beata0291 <beata.dabrowska91Qgmail.com>
2017-12-16 20:27:13 +0100

    metody pomocnicze dla grup

--- addressbook/src/test/java/ru/stqa/pft/addressbook/CreationGroupTest.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/CreationGroupTest.java
@@ -23,6 +23,10 @@ public class CreationGroupTest {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost:8080/addressbook/");
+        login();
+    }
+
+    private void login() {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys("admin");
@@ -31,14 +35,26 @@ public class CreationGroupTest {
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
-    
+
     @Test
     public void testCreationGroup() {
 
-        wd.findElement(By.linkText("groups")).click();
-        wd.findElement(By.name("new")).click();
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
+        goToGroupPage();
+        initGroupCreation();
+        fillGroupForm();
+        submitGroup();
+        returnToAllGroup();
+    }
+
+    private void returnToAllGroup() {
+        wd.findElement(By.linkText("group page")).click();
+    }
+
+    private void submitGroup() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void fillGroupForm() {
         wd.findElement(By.name("group_name")).sendKeys("Polska");
         wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
@@ -46,10 +62,18 @@ public class CreationGroupTest {
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
         wd.findElement(By.name("group_footer")).sendKeys("Kraków");
-        wd.findElement(By.name("submit")).click();
-        wd.findElement(By.linkText("group page")).click();
     }
-    
+
+    private void initGroupCreation() {
+        wd.findElement(By.name("new")).click();
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+    }
+
+    private void goToGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
