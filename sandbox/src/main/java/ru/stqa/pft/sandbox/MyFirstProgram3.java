c3fbda51503ec904573f47b54464ee4940c79ce7
beata0291 <beata.dabrowska91Qgmail.com>
2017-12-16 13:51:04 +0100

    Funcke do obliczania boków kwadratu i prostokąta

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram3.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram3.java
@@ -1,10 +1,36 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgram3
-{
+public class MyFirstProgram3 {
 
-	public static void main (String[] args){
-	System.out.println ("Hello, world!");
+	public static void main(String[] args) {
+		hello("world");
+		hello("user");
+		hello("Becia");
+
+		double len = 5;
+		System.out.println("Powierchnia kwadratu o boku " + len + "równa się " + area(len));
+
+		double a = 4;
+		double b = 3;
+		System.out.println("Powierchnia prostokąta o bokach " + a + " i " + b + " równa się " + area(a, b));
+
 	}
-}
+		public static void hello (String somebody){
+			System.out.println("Hello " + somebody + "!");
+		}
+
+		public static double area ( double l){
+			return l * l;
+		}
+
+	public static double area(double a, double b) {
+		return a*b;
+	}
+
+
+
+
+	}
+
+
 
