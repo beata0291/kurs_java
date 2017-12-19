6336f472f28c6a897f2090cc9efe36282752ef1d
beata0291 <beata.dabrowska91@gmail.com>
2017-12-19 22:26:24 +0100

    rename dla return page

--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/Address/CreationAddress.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/Address/CreationAddress.java
@@ -2,7 +2,6 @@ package ru.stqa.pft.addressbook.tests.Address;
 
 import org.testng.annotations.Test;
 
-import org.openqa.selenium.*;
 import ru.stqa.pft.addressbook.models.GroupAdressData;
 import ru.stqa.pft.addressbook.tests.TestBase;
 
@@ -16,40 +15,7 @@ public class CreationAddress extends TestBase {
         initAddressCreation();
         fillAddressForm(new GroupAdressData("Jan", "Kowalski", "Warszawa", "jan@wp.pl", "785456225455"));
         submitAddress();
-        returnAllAddress();
-    }
-
-    private void returnAllAddress() {
-        app.wd.findElement(By.linkText("home page")).click();
-    }
-
-    private void submitAddress() {
-        app.wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    }
-
-    private void fillAddressForm(GroupAdressData groupAdressData) {
-        app.wd.findElement(By.name("firstname")).sendKeys(groupAdressData.getFirstName());
-        app.wd.findElement(By.name("lastname")).click();
-        app.wd.findElement(By.name("lastname")).clear();
-        app.wd.findElement(By.name("lastname")).sendKeys(groupAdressData.getLastName());
-        app.wd.findElement(By.name("address")).click();
-        app.wd.findElement(By.name("address")).clear();
-        app.wd.findElement(By.name("address")).sendKeys(groupAdressData.getAddress());
-        app.wd.findElement(By.name("email")).click();
-        app.wd.findElement(By.name("email")).clear();
-        app.wd.findElement(By.name("email")).sendKeys(groupAdressData.getEmail());
-        app.wd.findElement(By.name("mobile")).click();
-        app.wd.findElement(By.name("mobile")).clear();
-        app.wd.findElement(By.name("mobile")).sendKeys(groupAdressData.getPhones());
-    }
-
-    private void initAddressCreation() {
-        app.wd.findElement(By.name("firstname")).click();
-        app.wd.findElement(By.name("firstname")).clear();
-    }
-
-    private void goToNewAddressPage() {
-        app.wd.findElement(By.linkText("add new")).click();
+        returnToHomePage();
     }
 
 
--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,9 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.helpers.ApplicationManager;
+import ru.stqa.pft.addressbook.models.GroupAdressData;
 
 public class TestBase {
 
@@ -19,4 +21,36 @@ public class TestBase {
         app.stop();
     }
 
+    protected void returnToHomePage() {
+        app.wd.findElement(By.linkText("home page")).click();
+    }
+
+    protected void submitAddress() {
+        app.wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    protected void fillAddressForm(GroupAdressData groupAdressData) {
+        app.wd.findElement(By.name("firstname")).sendKeys(groupAdressData.getFirstName());
+        app.wd.findElement(By.name("lastname")).click();
+        app.wd.findElement(By.name("lastname")).clear();
+        app.wd.findElement(By.name("lastname")).sendKeys(groupAdressData.getLastName());
+        app.wd.findElement(By.name("address")).click();
+        app.wd.findElement(By.name("address")).clear();
+        app.wd.findElement(By.name("address")).sendKeys(groupAdressData.getAddress());
+        app.wd.findElement(By.name("email")).click();
+        app.wd.findElement(By.name("email")).clear();
+        app.wd.findElement(By.name("email")).sendKeys(groupAdressData.getEmail());
+        app.wd.findElement(By.name("mobile")).click();
+        app.wd.findElement(By.name("mobile")).clear();
+        app.wd.findElement(By.name("mobile")).sendKeys(groupAdressData.getPhones());
+    }
+
+    protected void initAddressCreation() {
+        app.wd.findElement(By.name("firstname")).click();
+        app.wd.findElement(By.name("firstname")).clear();
+    }
+
+    protected void goToNewAddressPage() {
+        app.wd.findElement(By.linkText("add new")).click();
+    }
 }
