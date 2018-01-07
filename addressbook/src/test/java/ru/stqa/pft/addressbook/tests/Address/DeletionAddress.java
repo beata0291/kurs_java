1d153ce87a991d71a39e1bdd37ecdecb94997ecc
beata0291 <beata.dabrowska91@gmail.com>
2018-01-07 14:06:27 +0100

    warunki wstępne dla usuwania contaktu

--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/Address/DeletionAddress.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/Address/DeletionAddress.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
+import ru.stqa.pft.addressbook.models.GroupAdressData;
 import ru.stqa.pft.addressbook.tests.TestBase;
 
 public class DeletionAddress extends TestBase{
@@ -12,6 +13,10 @@ public class DeletionAddress extends TestBase{
     @Test
     public void testDeletionAddress() {
         app.getContactHelper().selectAddress();
+        if (! app.getContactHelper().isThereAContact()) {
+            app.getContactHelper().createContact(new GroupAdressData("Jan", "Kowalski", "Warszawa", "jan@wp.pl", "785456225455", null));
+        }
+
         app.getContactHelper().clickToDeleteAddress();
         app.getContactHelper().acceptToDeleteAddress();
 
