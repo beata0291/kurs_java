df19cced8b5bc66e1166593fb1ca24cbde04b66f
beata0291 <beata.dabrowska91@gmail.com>
2018-01-13 23:05:02 +0100

    Wyszukiwanie maksymalnego elementu w kolekcji

--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/Group/CreationGroupTest.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/Group/CreationGroupTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.models.GroupData;
 import ru.stqa.pft.addressbook.tests.TestBase;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class CreationGroupTest extends TestBase {
@@ -16,12 +17,23 @@ public class CreationGroupTest extends TestBase {
 
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("Polska", null, null));
+        GroupData group = new GroupData("Polska", null, null);
+        app.getGroupHelper().fillGroupForm(group);
 
         app.getGroupHelper().submitGroup();
         app.getGroupHelper().returnToAllGroup();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() +1);
+
+        int max = 0;
+        for (GroupData g : after) {
+            if (g.getId() > max) {
+                max = g.getId();
+                }
+            }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
 
     }
 
