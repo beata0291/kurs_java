81981a02789017bb89879ae67d4cc7afb6459bc5
beata0291 <beata.dabrowska91@gmail.com>
2018-01-13 15:20:39 +0100

    porównanie kolekcji

--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/Group/DeletionGroupTest.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/Group/DeletionGroupTest.java
@@ -22,6 +22,11 @@ public class DeletionGroupTest extends TestBase {
         app.getGroupHelper().returnToAllGroup();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() -1);
-    }
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
+
+        }
+
 
 }
