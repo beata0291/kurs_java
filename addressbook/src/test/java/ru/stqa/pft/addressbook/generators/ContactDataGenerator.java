5a3b204e3fdfbd153abb45e6b2f991b90c0eae71
beata0291 <beata.dabrowska91@gmail.com>
2018-02-08 22:01:58 +0100

    Lekcja 2 for contacts

---
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,41 @@
+package ru.stqa.pft.addressbook.generators;
+
+import ru.stqa.pft.addressbook.models.GroupAdressData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+    public static void main(String[] args) throws IOException {
+        int count = Integer.parseInt(args[0]);
+        File file = new File(args[1]);
+
+        List<GroupAdressData> contacts = generateContacts(count);
+        save(contacts, file);
+    }
+
+    private static void save(List<GroupAdressData> contacts, File file) throws IOException {
+        System.out.println(new File(".").getAbsolutePath());
+        Writer writer = new FileWriter(file);
+        for (GroupAdressData contact : contacts) {
+            writer.write(String.format("%s;%s;%s\n", contact.getFirstName(), contact.getLastName(), contact.getMobile()));
+        }
+        writer.close();
+
+    }
+
+    private static List<GroupAdressData> generateContacts(int count) {
+        List<GroupAdressData> contacts = new ArrayList<GroupAdressData>();
+        for (int i = 0; i < count; i++) {
+            contacts.add(new GroupAdressData().withFirstName(String.format("test %s", i))
+                    .withLastName(String.format("LastName %s", i)).withMobile(String.format("MobilePhone %s", i)));
+
+        }
+        return contacts;
+    }
+}
