7f5a1431d7b00090b1c3b7cccf045cdbc83c5124
beata0291 <beata.dabrowska91@gmail.com>
2018-02-09 18:33:53 +0100

    Lekcja 6.3 for contacts

--- addressbook/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.models.GroupAdressData;
 
 import java.io.File;
@@ -11,13 +14,33 @@ import java.util.List;
 
 public class ContactDataGenerator {
 
-    public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
 
-        List<GroupAdressData> contacts = generateContacts(count);
-        save(contacts, file);
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
     }
+
+    private void run() throws IOException {
+        List<GroupAdressData> contacts = generateContacts(count);
+        save(contacts, new File(file));
+
+    }
+
+
 
     private static void save(List<GroupAdressData> contacts, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
@@ -29,7 +52,7 @@ public class ContactDataGenerator {
 
     }
 
-    private static List<GroupAdressData> generateContacts(int count) {
+    private List<GroupAdressData> generateContacts(int count) {
         List<GroupAdressData> contacts = new ArrayList<GroupAdressData>();
         for (int i = 0; i < count; i++) {
             contacts.add(new GroupAdressData().withFirstName(String.format("test %s", i))
