87364d6db4c698f67100765728229d240db3dfbf
beata0291 <beata.dabrowska91@gmail.com>
2018-02-09 18:58:55 +0100

    Lekcja 6.5 for contacts

--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/Contact/CreationContact.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/Contact/CreationContact.java
@@ -6,7 +6,10 @@ import ru.stqa.pft.addressbook.models.Contacts;
 import ru.stqa.pft.addressbook.models.GroupAdressData;
 import ru.stqa.pft.addressbook.tests.TestBase;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -17,15 +20,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class CreationContact extends TestBase {
 
     @DataProvider
-   public Iterator<Object[]> validContacts() {
+    public Iterator<Object[]> validContacts() throws IOException {
             List<Object[]> list = new ArrayList<Object[]>();
-            File photo = new File("src/test/resources/stru.png");
-            list.add(new Object[]{new GroupAdressData().withFirstName("firtsName 1").withLastName("lastName 1")
-                            .withMobile("mobile 1").withEmail("email 1").withPhoto(photo)});
-            list.add(new Object[]{new GroupAdressData().withFirstName("firtsName 2").withLastName("lastName 2")
-                            .withMobile("mobile 2").withEmail("email 2").withPhoto(photo)});
-            list.add(new Object[]{new GroupAdressData().withFirstName("firtsName 3").withLastName("lastName 3")
-                            .withMobile("mobile 3").withEmail("email 3").withPhoto(photo)});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+            String line = reader.readLine();
+            while (line !=null) {
+                  String[] split = line.split(";");
+                  list.add(new Object[]{new GroupAdressData().withFirstName(split[0]).withLastName(split[1]).withMobile(split[2])
+                                  .withEmail(split[3])});
+                 line = reader.readLine();
+                }
             return list.iterator();
           }
 
